e0cf0f651a06b0a16a49b978fa3d895e59a7cb4d
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-09-29 17:37:46 +0200

    create variable initialState as an object for count and step, set state for the reducer of both

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -9,11 +9,11 @@ function reducer(state, action) {
 
 function DateCounter() {
   // const [count, setCount] = useState(0);
+  // const [step, setStep] = useState(1);
 
-  const [count, dispatch] = useReducer(reducer, 0);
-
-  const [step, setStep] = useState(1);
-
+  const initialState = { count: 0, step: 1 };
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { count, step } = state;
   // This mutates the date object.
   const date = new Date("june 21 2027");
   date.setDate(date.getDate() + count);
@@ -36,12 +36,12 @@ function DateCounter() {
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    // setStep(Number(e.target.value));
   };
 
   const reset = function () {
     // setCount(0);
-    setStep(1);
+    // setStep(1);
   };
 
   return (
