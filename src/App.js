1f028549e69e1cf732388a2713f9e1100bf28ec5
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-10-02 00:41:04 +0200

    create Main component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./Header";
+import Main from "./Main";
 
 export default function App() {
   return (
     <div className="app">
       <Header />
+      <Main />
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -3,10 +3,6 @@ function Header() {
     <header className="app-header">
       <img src="logo512.png" alt="React logo" />
       <h1>The React Quiz</h1>
-      <main className="main">
-        <p>1/15</p>
-        <p>Question?</p>
-      </main>
     </header>
   );
 }
