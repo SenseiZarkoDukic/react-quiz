1ec6d43d3a06a26b97074f0a77d939003f316286
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-10-02 00:34:06 +0200

    Add react logo image

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,12 @@
 function Header() {
   return (
-    <header className='app-header'>
-      <img src='logo512.png' alt='React logo' />
+    <header className="app-header">
+      <img src="logo512.png" alt="React logo" />
       <h1>The React Quiz</h1>
+      <main className="main">
+        <p>1/15</p>
+        <p>Question?</p>
+      </main>
     </header>
   );
 }
