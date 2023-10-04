8d9505a2b8b9c2eebb607c37e38fb98accb67579
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-10-04 15:35:49 +0200

    Add classNames btn & btn-ui for button in the Start Screen component

--- src/StartScreen.js
+++ src/StartScreen.js
@@ -3,7 +3,7 @@ function StartScreen({ numQuestions }) {
     <div className="start">
       <h2>Welcome to The React Quiz!</h2>
       <h3>{numQuestions} question to test your React mastery</h3>
-      <button>Let's start</button>
+      <button className="btn btn-ui">Let's start</button>
     </div>
   );
 }
