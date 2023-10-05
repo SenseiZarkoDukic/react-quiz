4d88204ed8c7da19c7634b959a512b791bdf6b2e
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-10-05 18:22:29 +0200

    Condition with status === 'finished' to get FinishScreen component UI

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import StartScreen from "./StartScreen";
 import Question from "./Question";
 import NextButton from "./NextButton";
 import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
 
 const initialState = {
   questions: [],
@@ -90,6 +91,9 @@ export default function App() {
             />
             <NextButton dispatch={dispatch} answer={answer} />
           </>
+        )}
+        {status === "finished" && (
+          <FinishScreen points={points} maxPossiblePoints={maxPossiblePoints} />
         )}
       </Main>
     </div>
---
+++ src/components/FinishScreen.js
@@ -0,0 +1,12 @@
+function FinishScreen({ points, maxPossiblePoints }) {
+  const percentage = (points / maxPossiblePoints) * 100;
+
+  return (
+    <p className="result">
+      You scored <strong>{points}</strong> out of {maxPossiblePoints} (
+      {Math.ceil(percentage)}%)
+    </p>
+  );
+}
+
+export default FinishScreen;
