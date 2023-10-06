c9d3b6fb271dc0c84fc89a42a0312904e6a29f7a
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-10-06 16:48:59 +0200

    Add emoji to the FinalScreen in condition to results in percentage

--- src/components/FinishScreen.js
+++ src/components/FinishScreen.js
@@ -1,8 +1,27 @@
 function FinishScreen({ points, maxPossiblePoints }) {
   const percentage = (points / maxPossiblePoints) * 100;
 
+  let emoji;
+
+  if (percentage === 100) {
+    emoji = "🥇";
+  }
+  if (percentage >= 80 && percentage < 100) {
+    emoji = "🥳";
+  }
+  if (percentage >= 50 && percentage < 80) {
+    emoji = "😊";
+  }
+  if (percentage > 0 && percentage < 50) {
+    emoji = "🤔";
+  }
+  if (percentage === 0) {
+    emoji = "🤦";
+  }
+
   return (
     <p className="result">
+      <span>{emoji}</span>
       You scored <strong>{points}</strong> out of {maxPossiblePoints} (
       {Math.ceil(percentage)}%)
     </p>
