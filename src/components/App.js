c3e8af5963bff4578f1f693ab3eb2529deb348fb
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-10-09 02:25:14 +0200

    Get minutes and seconds, condition them with being <0 to be 0

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,8 @@ import FinishScreen from "./FinishScreen";
 import Timer from "./Timer";
 import Footer from "./Footer";
 
+const SECS_PER_QUESTION = 10;
+
 const initialState = {
   questions: [],
 
@@ -20,7 +22,7 @@ const initialState = {
   answer: null,
   points: 0,
   highscore: 0,
-  secondsRemaining: 10,
+  secondsRemaining: null,
 };
 
 function reducer(state, action) {
@@ -30,7 +32,11 @@ function reducer(state, action) {
     case "dataFailed":
       return { ...state, status: "error" };
     case "start":
-      return { ...state, status: "active" };
+      return {
+        ...state,
+        status: "active",
+        secondsRemaining: state.questions.length * SECS_PER_QUESTION,
+      };
     case "newAnswer":
       const question = state.questions.at(state.index);
       return {
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 
 function Timer({ secondsRemaining, dispatch }) {
+  const minutes = String(Math.floor(secondsRemaining / 60)).padStart(2, 0);
+  const seconds = String(secondsRemaining % 60).padStart(2, 0);
+
   useEffect(() => {
     const id = setInterval(() => {
       dispatch({ type: "tick" });
@@ -10,7 +13,11 @@ function Timer({ secondsRemaining, dispatch }) {
   }, [dispatch]);
   return (
     <div className="timer">
-      <p>{secondsRemaining}</p>
+      <p>
+        {minutes < 0 && "0"}
+        {minutes}:{seconds < 0 && "0"}
+        {seconds}
+      </p>
     </div>
   );
 }
