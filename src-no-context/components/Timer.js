230ffb916452f32ee42ae4de289a3d19e143372f
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-11-01 13:44:53 +0100

    Create copy of src folder and name it src-no-context

---
+++ src-no-context/components/Timer.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+
+function Timer({ secondsRemaining, dispatch }) {
+  const minutes = String(Math.floor(secondsRemaining / 60)).padStart(2, 0);
+  const seconds = String(secondsRemaining % 60).padStart(2, 0);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      dispatch({ type: "tick" });
+    }, 1000);
+
+    return () => clearInterval(id);
+  }, [dispatch]);
+  return (
+    <div className="timer">
+      <p>
+        {minutes < 0 && "0"}
+        {minutes}:{seconds < 0 && "0"}
+        {seconds}
+      </p>
+    </div>
+  );
+}
+
+export default Timer;
+
+// const Timer = ({ initialTime }) => {
+//   const [time, setTime] = useState(initialTime);
+//   const seconds = String(time % 60).padStart(2, 0);
+//   const minutes = String(Math.floor(time / 60)).padStart(2, 0);
+
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       setTime((prevTime) => prevTime - 1);
+//     }, 1000);
+
+//     return () => clearInterval(interval);
+//   }, []);
+
+//   return (
+//     <div className="timer">
+//       <p>
+//         {minutes}:{seconds}
+//       </p>
+//     </div>
+//   );
+// };
+
+// export default Timer;
